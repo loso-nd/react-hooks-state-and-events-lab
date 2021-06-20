62c1d8bc9fc17ddf1ede46d0c31683aee0253abe
Nahum <losond8@gmail.com>
2021-06-20 14:25:45 -0700

    Adding state and event handler.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,25 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  let [toggleMode, setToggleMode] = useState(false)
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
+
+  const handleClick = () => {
+    
+    if(toggleMode === false){
+      console.log("Welcome to the Dark Side")
+      setToggleMode(true)
+    }
+
+  }
 
   return (
     <div className={"App " + (false ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={() =>handleClick()}>Dark Mode</button>
       </header>
       <ShoppingList items={items} />
     </div>
