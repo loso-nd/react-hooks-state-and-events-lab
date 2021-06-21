514c69dc617ac2d1b8dee581ab0198bc749f31b9
Nahum <losond8@gmail.com>
2021-06-20 19:20:25 -0700

    Added sudo code for explanation.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,22 +4,18 @@ import Item from "./Item";
 function ShoppingList({ items }) {  
   let [selectedCategory, setSelectedCategory] = useState("All")
 
+   // event.target.value will be the value selected by the user
   function handleChange(e){
-    console.log("Filter out: ", items)
+    console.log("Filter out: ", e.target.value)
     setSelectedCategory(e.target.value)
   }
 
-
   // we want to filter the items to only display the ones based on the selected category
-
     const itemsToDisplay = items.filter(item => {
       if (selectedCategory === "All"){
         return true
-      } else{
-        return item.category === selectedCategory
-      }
+      } else {return item.category === selectedCategory }
     })
-
   
   return (
     <div className="ShoppingList">
@@ -32,6 +28,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
+{/* repace items.map with itemsToDisplay bc it is the filtered version of the original state { items } passed in as props */}
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
